649582b5226fca45f509bb4288e1532905624125
Sahib Suri <surisahib7@gmail.com>
2026-06-17 23:11:07 +0530

    factorial

--- recursion/factorial.java
+++ recursion/factorial.java
@@ -2,12 +2,16 @@ package recursion;
 
 public class factorial {
     public static int calcFactorial(int n){
-        if(n<=1){           //base case
+        if(n<=1){
             return 1;
         }
-        return n*calcFactorial(n-1);            //recursive step
+        int res = n*calcFactorial(n-1);
+        System.out.println(" X " + n);
+        return res;
     }
     public static void main(String[] args) {
-        System.out.println(calcFactorial(5));
+        System.out.println("Factorial steps: ");
+        int result = calcFactorial(5);
+        System.out.println(" = " + result);
     }
 }
